a2ffa47200b4050a63c6ed6f606e4958b0158c18
rohitsoni-codelab <138715418+rohitsoni-codelab@users.noreply.github.com>
2023-07-06 00:14:38 +0530

    Add files via upload

---
+++ grades.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+    float m;
+    printf("Enter your overall percentage=");
+    scanf("%f", &m);
+    if (m > 40)
+    {
+        printf("Padh le bsdk");
+    }
+    if (m > 50)
+    {
+        printf("Thik hi hai but ladki se savdhan");
+    }
+    if (m > 60)
+    {
+        printf("Mahenat mangta hai appan ko");
+    }
+    if (m > 70)
+    {
+        printf("salary discuss kar le???");
+    }
+
+    if (m > 80)
+    {
+        printf("koi chaap nahi...");
+    }
+
+    if (m >= 90)
+    {
+        printf("App shubham ho naa???");
+    }
+
+    else
+    {
+        printf("Fuck off");
+    }
+
+    return 0;
+}
